10dc61d8f5c5338208234541b465479135814c27
Azam <azamdev00@gmail.com>
2023-07-07 16:42:45 +0500

    add modals

--- src/models/preset.ts
+++ src/models/preset.ts
@@ -1,1 +1,21 @@
+import { ObjectId } from "mongodb";
 
+export interface Stop {
+  _id: ObjectId;
+  ms: number;
+  sc: number;
+}
+
+export interface Preset {
+  _id: ObjectId;
+  forceRule: string;
+  forceType: string;
+  forceValue: number;
+  keepForce: boolean;
+  name: string;
+  selected: boolean;
+  showOnTap: boolean;
+  tapOnLap: boolean;
+  type: string;
+  value: string[];
+}
